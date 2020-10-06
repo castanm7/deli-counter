d9604bbf05b7e572687d43888e0a22cb4a53ac7d
Maria Castaneda <mariamcastaneda29@gmail.com>
2020-10-06 19:48:35 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+
+def line (katz_deli)
+    if katz_deli.size == 0 
+        puts "The line is currently empty."
+    elsif katz_deli.size >= 1 
+        counter = 1
+        katz_deli.each do |name|
+            array=[]
+            # counter_name = "#{counter}. #{name}"
+            array << "#{counter}. #{name}"
+            counter += 1
+        end
+        array.join" "
+        # puts "The line is currently: #{counter_name}"
+        # number = katz_deli.index + 1
+        # katz_deli_with_number = []
+        # katz_deli_with_number = katz_deli.unshift("#{number}")
+        # name_and_number=katz_deli_with_number.join(" ")
+        # puts "The line is currently: #{name_and_number}"
+    end
+end
